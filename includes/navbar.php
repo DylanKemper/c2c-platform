246c1fe104c368a9e46654bd664bff57351c1d3c
DylanKemper <dylankemper7@gmail.com>
2026-04-13 15:36:01 +0200

    Refactor navbar into a separate file and include it in the index page

---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container justify-content-center">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+            <div class="d-flex align-items-center">
+                <!-- Brand -->
+                <a class="navbar-brand me-4" href="#">Lootly</a>
+                <!-- Link -->
+                <div class="navbar-nav me-4">
+                    <a class="nav-link active" href="#">Categories</a>
+                </div>
+                <!-- Search -->
+                <form class="d-flex">
+                    <input class="form-control me-2 search-form" type="search" placeholder="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -14,27 +14,7 @@
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container justify-content-center">
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-            <div class="d-flex align-items-center">
-                <!-- Brand -->
-                <a class="navbar-brand me-4" href="#">Lootly</a>
-                <!-- Link -->
-                <div class="navbar-nav me-4">
-                    <a class="nav-link active" href="#">Categories</a>
-                </div>
-                <!-- Search -->
-                <form class="d-flex">
-                    <input class="form-control me-2 search-form" type="search" placeholder="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
-
+    <?php include 'includes/navbar.php'; ?>
     <h1>Bimbimbambam</h1>
 
 </body>
