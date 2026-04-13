783f78858f7facf296843a6ce437caadfe98d352
DylanKemper <dylankemper7@gmail.com>
2026-04-13 15:29:25 +0200

    Add initial HTML structure and CSS styles for the index page

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container justify-content-center">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+            <div class="d-flex align-items-center">
+                <!-- Brand -->
+                <a class="navbar-brand me-4" href="#">Lootly</a>
+                <!-- Link -->
+                <div class="navbar-nav me-4">
+                    <a class="nav-link active" href="#">Categories</a>
+                </div>
+                <!-- Search -->
+                <form class="d-flex">
+                    <input class="form-control me-2 search-form" type="search" placeholder="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
+
+    <h1>Bimbimbambam</h1>
+
+</body>
+
+</html>
